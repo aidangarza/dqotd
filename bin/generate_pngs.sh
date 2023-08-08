1ad15ce05a26dddf4c71ab4c6d4e202de98ee6c9
= <aidangarza@gmail.com>
2023-08-08 16:27:14 -0500

    Update svgs to use relative image path

--- bin/generate_pngs.sh
+++ bin/generate_pngs.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Directory containing the SVG files
-svg_directory="./path/to/svg/files"
+svg_directory="../public/quotes"
 
 # Loop through SVG files in the directory
 for svg_file in "$svg_directory"/*.svg; do
-  base_name=$(basename "$svg_file")
-  png_file="$base_name.png"
+  if [ -f "$svg_file" ]; then
+    base_name=$(basename "$svg_file")
+    png_file="svg_directory/${base_name%.svg}.png"
 
-  # Convert SVG to PNG using rsvg-convert
-  rsvg-convert -w 1024 "$svg_file" -o "$png_file"
+    # Convert SVG to PNG using rsvg-convert
+    rsvg-convert -w 1024 "$svg_file" -o "$png_file"
 
-  if [ $? -eq 0 ]; then
-    echo "Converted $svg_file to $png_file"
-  else
-    echo "Error converting $svg_file to PNG"
+    if [ $? -eq 0 ]; then
+      echo "Converted $svg_file to $png_file"
+    else
+      echo "Error converting $svg_file to PNG"
+    fi
   fi
 done
