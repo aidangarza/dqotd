45bf383b75d05ea087c7c3d47c09bb1406904883
Aidan Garza <aidangarza@gmail.com>
2026-06-08 17:32:16 -0500

    Update Acorns promotion URL

--- components/promotion.tsx
+++ components/promotion.tsx
@@ -3,7 +3,7 @@ import promotionStyles from './promotion-styles.module.css'
 const PROMOTIONS = [
   {
     name: 'acorns_a',
-    url: 'https://share.acorns.com/aidangarza',
+    url: 'https://acorns.com/share/?shareable_code=CZZXKLB&first_name=Aidan&friend_reward=5',
     text: "Walt wasn't good with money. His big brother Roy always came to his rescue. If you don't have a Roy, consider building your saving habits with Acorns.",
     cta: 'Click here to learn more.',
   },
